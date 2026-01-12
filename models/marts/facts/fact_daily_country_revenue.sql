130881c55ddc7cc402dd0372bd27676e4275370e
Roee Cohen <roeecohen@h-MacBook-Pro-sl-Roee.local>
2026-01-12 10:03:54 +0200

    Added fact_daily_country_revenue

--- models/marts/facts/fact_daily_country_revenue.sql
+++ models/marts/facts/fact_daily_country_revenue.sql
@@ -5,10 +5,13 @@
 
 SELECT
   DATE(oi.created_at) AS order_date,
+  u.country as country,
   COUNT(DISTINCT oi.order_id) AS total_orders,
   COUNT(oi.order_item_id) AS total_items_sold,
   ROUND(SUM(oi.sale_price), 2) AS daily_revenue,
   ROUND(AVG(oi.sale_price), 2) AS avg_item_price,
   (ROUND(SUM(oi.sale_price) / COUNT(DISTINCT oi.order_id), 2)) as aov
 FROM {{ref('stg_order_items')}} oi
-GROUP BY DATE(oi.created_at)
+JOIN {{ref('stg_users')}} u
+ON u.user_id = oi.user_id
+GROUP BY DATE(oi.created_at), u.country
